f4f45df01de6ba790f19ef06681390143cd2d10b
LiuSmile <liusmile1314@outlook.com>
2015-10-13 23:19:47 +0800

    2015-10-13 23:19:44

--- ProxyStu/Program.cs
+++ ProxyStu/Program.cs
@@ -10,7 +10,115 @@ namespace ProxyStu
     {
         static void Main(string[] args)
         {
-            //2015年10月13日22:37:12。
+            Proxy proxyObj = new Proxy();
+            proxyObj.Request();
+            Console.ReadLine();
+
+            Console.WriteLine("********************");
+
+            ProxyHuman proxyHuman = new ProxyHuman(new SchoolGirl() { Name = "HanMeimei" });
+            proxyHuman.GiveChocolate();
+            proxyHuman.GiveDolls();
+            Console.ReadLine();
         }
     }
+
+    abstract class Subject
+    {
+        public abstract void Request();
+    }
+
+    class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Real Subject Request");
+        }
+    }
+
+    class Proxy : Subject
+    {
+        Subject realSubject;
+
+        //public Proxy()
+        //{
+        //    realSubject = new RealSubject();
+        //}
+
+        public override void Request()
+        {
+            if (realSubject == null)
+            {
+                realSubject = new RealSubject();
+            }
+            realSubject.Request();
+        }
+    }
+
+
+    /************************/
+
+    interface IGiveGift
+    {
+        void GiveDolls();
+        void GiveFlowers();
+        void GiveChocolate();
+    }
+
+    class SchoolGirl
+    {
+        public string Name {get;set;}
+    }
+
+    class Pursuit : IGiveGift
+    {
+        SchoolGirl mm;
+
+        public Pursuit(SchoolGirl mm)
+        {
+            this.mm = mm;
+        }
+
+        public void GiveDolls()
+        {
+            Console.WriteLine(mm.Name + " Give You Dolls");
+        }
+
+        public void GiveFlowers()
+        {
+            Console.WriteLine(mm.Name + " Give You Flowers");
+        }
+
+        public void GiveChocolate()
+        {
+            Console.WriteLine(mm.Name + " Give You Chocolates");
+        }
+    }
+
+    class ProxyHuman : IGiveGift
+    {
+        Pursuit pursuit;
+
+        public ProxyHuman(SchoolGirl mm)
+        {
+            pursuit = new Pursuit(mm);
+        }
+
+        public void GiveDolls()
+        {
+            pursuit.GiveDolls();
+        }
+
+        public void GiveFlowers()
+        {
+            pursuit.GiveFlowers();
+        }
+
+        public void GiveChocolate()
+        {
+            pursuit.GiveChocolate();
+        }
+    }
+
+
 }
